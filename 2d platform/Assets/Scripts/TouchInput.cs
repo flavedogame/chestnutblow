9be6eb29329f242b25aaacace30e46a500048efc
shshwdr <cs.vivian04@gmail.com>
2015-12-23 09:58:18 -0800

    2d sample and ui sample
    most of them are useless and will be delete later
    menu and mainGame are two first scenes for the game

---
+++ 2d platform/Assets/Scripts/TouchInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour {
+
+    public GameObject windPrefab;
+    public GameObject chestnutGo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+
+            Vector3 mousePosition = Input.mousePosition;
+           // Instantiate(windPrefab, mousePosition, Quaternion.identity);
+            Vector3 chestnutPosition =  Camera.main.WorldToScreenPoint(chestnutGo.transform.position);
+            //Debug.Log(chestnutPosition + " "+ mousePosition);
+            chestnutGo.GetComponent<Rigidbody2D>().AddForce( (chestnutPosition - mousePosition) * Time.deltaTime*0.5f);
+
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Vector3 mousePosition = Input.GetTouch(0).position;
+            // Instantiate(windPrefab, mousePosition, Quaternion.identity);
+            Vector3 chestnutPosition = Camera.main.WorldToScreenPoint(chestnutGo.transform.position);
+           // Debug.Log(chestnutPosition + " " + mousePosition);
+            chestnutGo.GetComponent<Rigidbody2D>().AddForce((chestnutPosition - mousePosition) * Time.deltaTime * 0.5f);
+        }
+	}
+}
