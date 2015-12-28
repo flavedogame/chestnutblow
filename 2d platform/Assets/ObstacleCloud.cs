bbc4ce8e497a22f46152f23f5e047e6a14b3220b
shshwdr <cs.vivian04@gmail.com>
2015-12-28 07:58:01 -0800

    add cloud
    player slow down when hit the cloud. the effect is not that obvious

---
+++ 2d platform/Assets/ObstacleCloud.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleCloud : BaseObstacle{
+    private Vector3 dropVector = new Vector3(0, 2, 0);
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (dropVector) * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            //it should do some animation and/or play sound so player knows the cloud does something
+
+            other.gameObject.GetComponent<Rigidbody2D>().mass = 4;
+            other.gameObject.GetComponent<Rigidbody2D>().gravityScale =-0.02f;
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(other.gameObject.GetComponent<Rigidbody2D>().velocity.x/2,0);
+            
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            // coll.gameObject.SendMessage("ApplyDamage", 10);
+
+
+            other.gameObject.GetComponent<Rigidbody2D>().mass = 1;
+        other.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.02f;
+
+    }
+}
