6f728fa0294ff63f7ddfc82d1b5455432f11b7bc
shshwdr <cs.vivian04@gmail.com>
2015-12-28 08:11:00 -0800

    add red dot at touch place
    for testing and later multiplayer

--- 2d platform/Assets/Scripts/TouchInput.cs
+++ 2d platform/Assets/Scripts/TouchInput.cs
@@ -6,6 +6,10 @@ public class TouchInput : MonoBehaviour {
     public GameObject windPrefab;
     public GameObject chestnutGo;
 
+    public GameObject fingerPointPrefab;
+    private Vector3 fingerPointDefaultPosition = new Vector3(100, 100, 0);
+    private Vector3 fingerPointZAdjust = new Vector3(0, 0, 10);
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,23 +17,29 @@ public class TouchInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetMouseButton(0))
+        {
 
             Vector3 mousePosition = Input.mousePosition;
-           // Instantiate(windPrefab, mousePosition, Quaternion.identity);
-            Vector3 chestnutPosition =  Camera.main.WorldToScreenPoint(chestnutGo.transform.position);
+            // Instantiate(windPrefab, mousePosition, Quaternion.identity);
+            Vector3 chestnutPosition = Camera.main.WorldToScreenPoint(chestnutGo.transform.position);
             //Debug.Log(chestnutPosition + " "+ mousePosition);
             chestnutGo.GetComponent<Rigidbody2D>().AddForce((chestnutPosition - mousePosition).normalized * Time.deltaTime * 115f);
-
+            fingerPointPrefab.transform.position = Camera.main.ScreenToWorldPoint(mousePosition) + fingerPointZAdjust;
         }
 
-        if (Input.touchCount > 0)
+
+        else if (Input.touchCount > 0)
         {
             Vector3 mousePosition = Input.GetTouch(0).position;
             // Instantiate(windPrefab, mousePosition, Quaternion.identity);
             Vector3 chestnutPosition = Camera.main.WorldToScreenPoint(chestnutGo.transform.position);
-           // Debug.Log(chestnutPosition + " " + mousePosition);
+            // Debug.Log(chestnutPosition + " " + mousePosition);
             chestnutGo.GetComponent<Rigidbody2D>().AddForce((chestnutPosition - mousePosition).normalized * Time.deltaTime * 115f);
+            fingerPointPrefab.transform.position = Camera.main.ScreenToWorldPoint(mousePosition) + fingerPointZAdjust;
+        }
+        else {
+            fingerPointPrefab.transform.position = fingerPointDefaultPosition;
         }
 	}
 }
