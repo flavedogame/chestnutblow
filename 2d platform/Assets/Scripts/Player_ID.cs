5cb178c8d48a3a3f8f2806e83fd93179de7f9443
shshwdr <cs.vivian04@gmail.com>
2015-12-29 20:42:43 -0800

    player(finger point) has unique name
    "Player "+netID

---
+++ 2d platform/Assets/Scripts/Player_ID.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_ID : NetworkBehaviour {
+
+    [SyncVar]
+    public string playerUniqueName;
+    private NetworkInstanceId playerNetID;
+    private Transform myTransform;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetNetIdentity();
+        SetIdentity();
+    }
+
+	// Use this for initialization
+	void Awake () {
+        myTransform = transform;
+	}
+
+    [Client]
+    void GetNetIdentity()
+    {
+        playerNetID = GetComponent<NetworkIdentity>().netId;
+        CmdTellServerMyIdentity(MakeUniqueName());
+    
+    }
+
+    [Client]
+    void SetIdentity() {
+        if (isLocalPlayer)
+        {
+            myTransform.name = MakeUniqueName();
+
+        }
+        else {
+            myTransform.name = playerUniqueName;
+        }
+    }
+
+    string MakeUniqueName() {
+        string uniqueName = "Player " + playerNetID.ToString();
+        return uniqueName;
+    }
+
+    [Command]
+    void CmdTellServerMyIdentity(string name)
+    {
+        playerUniqueName = name;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (myTransform.name == "" || myTransform.name == "fingerPoint(Clone)") {
+            SetIdentity();
+        }
+	}
+}
